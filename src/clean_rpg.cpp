60ade6da16da0cc5c33acd3d8fd2f5d62a146358
Matthias Hölzl <tc@xantira.com>
2021-05-09 01:34:20 +0200

    Add window with event loop

--- src/clean_rpg.cpp
+++ src/clean_rpg.cpp
@@ -1,14 +1,22 @@
 // Copyright (c) 2021 Dr. Matthias Hölzl. See file LICENSE.md.
 
-#include <clean_rpg_config.hpp>
+#include "SFML/Window.hpp"
+#include "clean_rpg_config.hpp"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Clean RPG Version "
-              << CLEAN_RPG_VERSION_MAJOR << "."
-              << CLEAN_RPG_VERSION_MINOR << "."
-              << CLEAN_RPG_VERSION_PATCH << " " 
-              << "is up and running!" << std::endl;
-    return 0;
+int main() {
+  std::cout << "Clean RPG Version " << CLEAN_RPG_VERSION_MAJOR << "."
+            << CLEAN_RPG_VERSION_MINOR << "." << CLEAN_RPG_VERSION_PATCH << " "
+            << "is up and running!" << std::endl;
+
+  sf::Window window{sf::VideoMode{800, 600}, "Clean RPG"};
+  while (window.isOpen()) {
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+  }
+  return 0;
 }
